5cfc65b0aaed6101013a568899d0ba1568349b95
Salvador Alvarez <sao.95@hotmail.com>
2022-05-18 15:07:57 -0700

    solucion de la tercera actividad

---
+++ 03-JS-II/homework/resolver.js
@@ -0,0 +1,21 @@
+// # Homework: Javascript II
+
+// ## Instrucciones
+// ---
+// 1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+// -------------------------Respuesta-----------------
+//bueno primeramente un ciclo for es muy usado al momento que queremos recorrer una lista o array en ingles ya que una de sus ventajas es que  aveces estas suelen tener muchos datos y queremos visualizar su informacion este es de gran ayuda para grandes datos  
+
+
+// * `for`
+// * `&&`, `||`, `!`
+
+// -------------------------Respuesta-----------------
+//el and , or y el not son muy utlizados al momento de quere comparar nuestros datos el and sirve cuando solo las dos codiciones se cumple el or siempre y cuanod una de las dos codiones se cumpla el simbolo de esclamacion ! o not srive para negar una condicion  
+
+
+
+// 2. Desde la carpeta `Prep` en la carpeta donde clonaste el repo, ingresa el comando `npm test JSII.test.js` para correr los tests automatizados. Al principio, todos los tests estarán fallados/rotos. Encontrarás las funciones para hacer pasar los tests en el archivo `homework.js`.
+
+// ### Aca tendras acceso a las [Soluciones](https://github.com/atralice/Curso.Prep.Henry/blob/solution/03-JS-II/homework/homework.js)
