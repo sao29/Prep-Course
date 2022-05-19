675c661a512db323bebec777fba8abc398099f0b
Salvador Alvarez <sao.95@hotmail.com>
2022-05-18 22:42:52 -0700

    se soluciono las actividades de objetos de js IV

---
+++ 05-JS-IV/homework/respuesta.js
@@ -0,0 +1,11 @@
+// 1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+// 	* Objetos un objeto se encarga de almacenar varios valores casi igual que un array , pero a diferencia de estos lo guarda con una llave y valor es parecido a los diccionarios de otros lenguajes ya que cada es una manera de orignazar nuestra datos muchas vecese usado en JSON 
+// 	* Propiedades
+//     las propedas son el valor que se les da a las llaves si hablamos de un auto tiene sus propiedades como el coloy el tamaño etc 
+// 	* Métodos : se puede consierar como metodo a una funcion que se encuentra dentro de un objeto basicamente son propiedades de un objeto 
+     
+// 	* Bucle `for…in` : el bluce for in es una forma para recorrer un areglo o un objeto digamos que queremos que realice nuestro ciclo for un recorrido a un array con el for el nomas declraron una varible y despes del in ingresamos que se desear recorrer y ya despues con un console log podemos ver toda la info de una forma mas facil y rapida 
+// 	* Notación de puntos vs notación de corchetes
+    
+//     respuesta: la notacion de punto es mas rapido de escribir pero la notacion de corchetes se puede usar para accedr a las propiedades con caracteres especiales 
